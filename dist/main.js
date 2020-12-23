1ffb91db795e0adbfaa2fcc03814ad39b034f259
Cielpy <beijiu572@gmail.com>
2020-12-23 00:55:39 +0000

    Update trackers

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MagnetTrackerComplement
 // @namespace    https://github.com/cielpy/MagnetTrackerComplement
-// @version      0.24
+// @version      0.25
 // @description  给网页上的磁链添加 tracker，tracker 来源 https://ngosang.github.io/trackerslist/trackers_best.txt
 // @author       cielpy
 // @include      http://*
@@ -14,7 +14,7 @@
 
 (function () {
     function appendTrackers(url) {
-        let trackerList = ["udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce","udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce","udp%3A%2F%2F9.rarbg.me%3A2710%2Fannounce","udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce","udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce","udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce","udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce","udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce","http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce","udp%3A%2F%2Fp4p.arenabg.ch%3A1337%2Fannounce","udp%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce","http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce","udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce","udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce","udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce","udp%3A%2F%2Ftracker.ds.is%3A6969%2Fannounce","http%3A%2F%2Fopen.acgnxtracker.com%3A80%2Fannounce","udp%3A%2F%2Ftracker.zerobytes.xyz%3A1337%2Fannounce","udp%3A%2F%2Fretracker.lanta-net.ru%3A2710%2Fannounce","udp%3A%2F%2Fopen.demonii.si%3A1337%2Fannounce"]
+        let trackerList = ["udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce","udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce","udp%3A%2F%2F9.rarbg.me%3A2710%2Fannounce","udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce","udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce","udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce","udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce","udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce","udp%3A%2F%2Fp4p.arenabg.ch%3A1337%2Fannounce","http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce","udp%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce","http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce","udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce","udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce","udp%3A%2F%2Ftracker.zerobytes.xyz%3A1337%2Fannounce","udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce","udp%3A%2F%2Ftracker.ds.is%3A6969%2Fannounce","udp%3A%2F%2Fretracker.lanta-net.ru%3A2710%2Fannounce","udp%3A%2F%2Fopen.demonii.si%3A1337%2Fannounce","http%3A%2F%2Fopen.acgnxtracker.com%3A80%2Fannounce"]
         var trackerListUrlPrams = "&tr=" + trackerList.join("&tr=");
 
         const r = /(?<=dn\=)(.*?)(?=&)/
