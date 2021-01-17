62c356163a85f92625edc521936e860148abab3c
Cielpy <beijiu572@gmail.com>
2021-01-17 10:41:16 +0000

    Update trackers

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MagnetTrackerComplement
 // @namespace    https://github.com/cielpy/MagnetTrackerComplement
-// @version      0.54
+// @version      0.55
 // @description  给网页上的磁链追加优质 tracker
 // @author       cielpy
 // @include      http://*
